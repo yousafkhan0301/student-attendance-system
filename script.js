c9b87a56491fa1adb5d20f73f4e8f1d23b71f022
yousafkhan0301 <m.yousaf.khan.0301@gmail.com>
2025-12-24 18:09:28 +0500

    Update script.js

--- script.js
+++ script.js
@@ -176,3 +176,11 @@ function downloadMonthlyReport() {
   showMessage("Monthly report downloaded successfully");
 }
 
+
+window.addEventListener("load", () => {
+  const today = new Date();
+  const yyyy = today.getFullYear();
+  const mm = String(today.getMonth() + 1).padStart(2, '0');
+  const dd = String(today.getDate()).padStart(2, '0');
+  document.getElementById("attendanceDate").value = `${yyyy}-${mm}-${dd}`;
+});
